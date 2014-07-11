bba3ac5bfeb5c252583721bba0144b0197ecdc3f
Udit Desai <desaiuditd@gmail.com>
2014-07-11 19:26:19 +0530

    Conversion : CRM => Helpdesk : class-rt-crm-utils.php

--- app/helper/class-rt-crm-utils.php
+++ app/helper/class-rt-crm-utils.php
@@ -1,8 +1,9 @@
 <?php
+
 /**
  * Don't load this file directly!
  */
-if (!defined('ABSPATH'))
+if ( ! defined( 'ABSPATH' ) )
 	exit;
 
 /*
@@ -12,57 +13,62 @@ if (!defined('ABSPATH'))
  */
 
 /**
- * Description of Rt_CRM_Utils
+ * Description of Rt_HD_Utils
  *
  * @author udit
  */
-if( !class_exists( 'Rt_CRM_Utils' ) ) {
-	class Rt_CRM_Utils {
-		static public function forceUFT8($tmpStr){
-			return preg_replace('/[^(\x20-\x7F)]*/','', $tmpStr);
+if ( ! class_exists( 'Rt_HD_Utils' ) ) {
+
+	class Rt_HD_Utils {
+
+		static public function forceUFT8( $tmpStr ) {
+			return preg_replace( '/[^(\x20-\x7F)]*/', '', $tmpStr );
 		}
+
 		public static $mime_types = array(
-				"pdf" => "application/pdf"
-				, "exe" => "application/octet-stream"
-				, "zip" => "application/zip"
-				, "docx" => "application/msword"
-				, "doc" => "application/msword"
-				, "xls" => "application/vnd.ms-excel"
-				, "ppt" => "application/vnd.ms-powerpoint"
-				, "gif" => "image/gif"
-				, "png" => "image/png"
-				, "jpeg" => "image/jpg"
-				, "jpg" => "image/jpg"
-				, "mp3" => "audio/mpeg"
-				, "wav" => "audio/x-wav"
-				, "mpeg" => "video/mpeg"
-				, "mpg" => "video/mpeg"
-				, "mpe" => "video/mpeg"
-				, "mov" => "video/quicktime"
-				, "avi" => "video/x-msvideo"
-				, "3gp" => "video/3gpp"
-				, "css" => "text/css"
-				, "jsc" => "application/javascript"
-				, "js" => "application/javascript"
-				, "php" => "text/html"
-				, "htm" => "text/html"
-				, "html" => "text/html"
-			);
-		static public function log($msg,$filename="error_log.txt") {
-			$log_file = "/tmp/rtcrm_" . $filename;
-			if ($fp = fopen($log_file, "a+")) {
-				fwrite($fp, "\n" . '[' . date(DATE_RSS) . '] ' . $msg . "\n");
-				fclose($fp);
+			"pdf" => "application/pdf"
+			, "exe" => "application/octet-stream"
+			, "zip" => "application/zip"
+			, "docx" => "application/msword"
+			, "doc" => "application/msword"
+			, "xls" => "application/vnd.ms-excel"
+			, "ppt" => "application/vnd.ms-powerpoint"
+			, "gif" => "image/gif"
+			, "png" => "image/png"
+			, "jpeg" => "image/jpg"
+			, "jpg" => "image/jpg"
+			, "mp3" => "audio/mpeg"
+			, "wav" => "audio/x-wav"
+			, "mpeg" => "video/mpeg"
+			, "mpg" => "video/mpeg"
+			, "mpe" => "video/mpeg"
+			, "mov" => "video/quicktime"
+			, "avi" => "video/x-msvideo"
+			, "3gp" => "video/3gpp"
+			, "css" => "text/css"
+			, "jsc" => "application/javascript"
+			, "js" => "application/javascript"
+			, "php" => "text/html"
+			, "htm" => "text/html"
+			, "html" => "text/html"
+		);
+
+		static public function log( $msg, $filename = "error_log.txt" ) {
+			$log_file = "/tmp/rt_helpdesk" . $filename;
+			if ( $fp = fopen( $log_file, "a+" ) ) {
+				fwrite( $fp, "\n" . '[' . date( DATE_RSS ) . '] ' . $msg . "\n" );
+				fclose( $fp );
 			}
 		}
 
-		static public function setAccounts($rCount){
-			$log_file = RT_CRM_PATH . "mailaccount.txt";
-			if ($fp = fopen($log_file, "w+")) {
-				fwrite($fp,$rCount);
-				fclose($fp);
+		static public function setAccounts( $rCount ) {
+			$log_file = RT_HD_PATH . "mailaccount.txt";
+			if ( $fp = fopen( $log_file, "w+" ) ) {
+				fwrite( $fp, $rCount );
+				fclose( $fp );
 			}
 		}
+
 		/**
 		 * Determine if a post exists based on title, content, and date
 		 *
@@ -73,61 +79,62 @@ if( !class_exists( 'Rt_CRM_Utils' ) ) {
 		 * @param string $date Optional post date
 		 * @return int Post ID if post exists, 0 otherwise.
 		 */
-		static public function post_exists($title, $content = '', $date = '') {
+		static public function post_exists( $title, $content = '', $date = '' ) {
 			global $wpdb;
 
-			$post_title = stripslashes(sanitize_post_field('post_title', $title, 0, 'db'));
-			$post_content = stripslashes(sanitize_post_field('post_content', $content, 0, 'db'));
-			$post_date = stripslashes(sanitize_post_field('post_date', $date, 0, 'db'));
+			$post_title = stripslashes( sanitize_post_field( 'post_title', $title, 0, 'db' ) );
+			$post_content = stripslashes( sanitize_post_field( 'post_content', $content, 0, 'db' ) );
+			$post_date = stripslashes( sanitize_post_field( 'post_date', $date, 0, 'db' ) );
 
 			$query = "SELECT ID FROM $wpdb->posts WHERE 1=1";
 			$args = array();
 
-			if (!empty($date)) {
+			if ( ! empty( $date ) ) {
 				$query .= ' AND post_date = %s';
 				$args[] = $post_date;
 			}
 
-			if (!empty($title)) {
+			if ( ! empty( $title ) ) {
 				$query .= ' AND post_title = %s';
 				$args[] = $post_title;
 			}
 
-			if (!empty($content)) {
+			if ( ! empty( $content ) ) {
 				$query .= 'AND post_content = %s';
 				$args[] = $post_content;
 			}
 
-			if (!empty($args))
-				return $wpdb->get_var($wpdb->prepare($query, $args));
+			if ( ! empty( $args ) )
+				return $wpdb->get_var( $wpdb->prepare( $query, $args ) );
 			return 0;
 		}
 
-		public static function get_crm_rtcamp_user() {
-			$users = rt_biz_get_module_users( RT_CRM_TEXT_DOMAIN );
+		public static function get_hd_rtcamp_user() {
+			$users = rt_biz_get_module_users( RT_HD_TEXT_DOMAIN );
 			return $users;
 		}
 
-		public static function get_mime_type($file) {
+		public static function get_mime_type( $file ) {
 
 			// our list of mime types
 
-			$extension = strtolower(end(explode('.', $file)));
-			if(isset(Rt_CRM_Utils::$mime_types[$extension]))
-				return Rt_CRM_Utils::$mime_types[$extension];
+			$extension = strtolower( end( explode( '.', $file ) ) );
+			if ( isset( self::$mime_types[ $extension ] ) )
+				return self::$mime_types[ $extension ];
 			else {
 				return "application/octet-stream";
 			}
 		}
 
-		public static function get_extention($file){
+		public static function get_extention( $file ) {
 
-			foreach(Rt_CRM_Utils::$mime_types as $key=>$mime){
-				if($mime== $file)
+			foreach ( self::$mime_types as $key => $mime ) {
+				if ( $mime == $file )
 					return $key;
 			}
 			return "tmp";
-
 		}
+
 	}
+
 }
