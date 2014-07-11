9407346a63b936ca74a9072cefede81b2870b87d
Udit Desai <desaiuditd@gmail.com>
2014-07-11 20:04:31 +0530

    Conversion : CRM => Helpdesk : class-rt-crm-leads-front.php

--- app/class-rt-crm-leads-front.php
+++ app/class-rt-crm-leads-front.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Don't load this file directly!
  */
@@ -12,64 +13,68 @@ if ( ! defined( 'ABSPATH' ) )
  */
 
 /**
- * Description of Rt_CRM_Leads_Front
+ * Description of Rt_HD_Tickets_Front
  *
  * @author udit
  */
-if ( ! class_exists( 'Rt_CRM_Leads_Front' ) ) {
-	class Rt_CRM_Leads_Front {
+if ( ! class_exists( 'Rt_HD_Tickets_Front' ) ) {
+
+	class Rt_HD_Tickets_Front {
+
 		public function __construct() {
 			add_filter( 'template_include', array( $this, 'template_include' ), 1, 1 );
-			add_filter( 'wp_title', array( $this, 'change_title'), 9999, 1 );
+			add_filter( 'wp_title', array( $this, 'change_title' ), 9999, 1 );
 		}
 
 		function change_title( $title ) {
-			global $rtcrm_front_page_title;
-			if( isset( $rtcrm_front_page_title ) && !empty( $rtcrm_front_page_title ) ) {
-				return $rtcrm_front_page_title;
+			global $rthd_front_page_title;
+			if ( isset( $rthd_front_page_title ) && ! empty( $rthd_front_page_title ) ) {
+				return $rthd_front_page_title;
 			}
 			return $title;
 		}
 
 		function template_include( $template ) {
-			global $wp_query, $rt_crm_module;
+			global $wp_query, $rt_hd_module;
 
-			if ( !isset($wp_query->query_vars['name']) ) {
+			if ( ! isset( $wp_query->query_vars[ 'name' ] ) ) {
 				return $template;
 			}
 
-			$name = $wp_query->query_vars['name'];
+			$name = $wp_query->query_vars[ 'name' ];
 
-			$post_type = rtcrm_post_type_name( $name );
-			if( $post_type != $rt_crm_module->post_type ) {
+			$post_type = rthd_post_type_name( $name );
+			if ( $post_type != $rt_hd_module->post_type ) {
 				return $template;
 			}
 
-			if( ! isset( $_REQUEST['rtcrm_unique_id'] ) || ( isset( $_REQUEST['rtcrm_unique_id'] ) && empty( $_REQUEST['rtcrm_unique_id'] ) ) ) {
+			if ( ! isset( $_REQUEST[ 'rthd_unique_id' ] ) || ( isset( $_REQUEST[ 'rthd_unique_id' ] ) && empty( $_REQUEST[ 'rthd_unique_id' ] ) ) ) {
 				return $template;
 			}
 
 			$args = array(
-				'meta_key' => 'rtcrm_unique_id',
-				'meta_value' => $_REQUEST['rtcrm_unique_id'],
+				'meta_key' => 'rthd_unique_id',
+				'meta_value' => $_REQUEST[ 'rthd_unique_id' ],
 				'post_status' => 'any',
 				'post_type' => $post_type,
 			);
 
-			$leadpost = get_posts( $args );
-			if( empty( $leadpost ) ) {
+			$ticketpost = get_posts( $args );
+			if ( empty( $ticketpost ) ) {
 				return $template;
 			}
-			$lead = $leadpost[0];
-			if( $post_type != $lead->post_type ) {
+			$ticket = $ticketpost[ 0 ];
+			if ( $post_type != $ticket->post_type ) {
 				return $template;
 			}
 
-			global $rtcrm_lead;
-			$rtcrm_lead = $lead;
-			global $rtcrm_front_page_title;
-			$rtcrm_front_page_title = ucfirst( $name ).' | '.$rtcrm_lead->post_title;
-			return rtcrm_locate_template( 'lead-front-page.php' );
+			global $rthd_ticket;
+			$rthd_ticket = $ticket;
+			global $rthd_front_page_title;
+			$rthd_front_page_title = ucfirst( $name ) . ' | ' . $rthd_ticket->post_title;
+			return rthd_locate_template( 'ticket-front-page.php' );
 		}
+
 	}
+
 }
