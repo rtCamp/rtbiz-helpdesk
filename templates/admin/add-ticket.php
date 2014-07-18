5b0976ded621171f63d5738076721148c5f0352c
Udit Desai <desaiuditd@gmail.com>
2014-07-18 16:49:56 +0530

    Helpdesk Extra info hook changed

--- templates/admin/add-ticket.php
+++ templates/admin/add-ticket.php
@@ -1024,7 +1024,7 @@ if( !empty( $results ) ) {
 			<?php $rt_hd_attributes->render_meta( $attr, isset($post->ID) ? $post->ID : '', $user_edit ); ?>
 		</div>
 	<?php } ?>
-	<?php do_action( 'rt_hd_after_ticket_information', $post->ID, $user_edit ); ?>
+	<?php do_action( 'rt_hd_after_ticket_information', $post, $user_edit ); ?>
 	</div>
 </div>
 			<?php do_action( 'rt_hd_other_details', $user_edit, $post ); ?>
