e5a8208dac3bda0b162f17386fe470ec447e3dde
Udit Desai <desaiuditd@gmail.com>
2014-11-07 19:43:44 +0530

    Followup date fix

--- app/helper/rthd-functions.php
+++ app/helper/rthd-functions.php
@@ -543,7 +543,7 @@ function rthd_render_comment( $comment, $user_edit, $type = 'right', $echo = tru
 
 			<time title="<?php echo esc_attr( $comment->comment_date ); ?>" datetime="<?php echo esc_attr( $comment->comment_date ); ?>">
 				<span title="<?php echo esc_attr( ( $comment->comment_author_email == '' ) ? $comment->comment_author_IP : $comment->comment_author_email ); ?>"><?php echo esc_attr( ( $comment->comment_author == '' ) ? 'Anonymous' : $comment->comment_author ); ?> </span>
-				&middot; <?php echo esc_attr( human_time_diff( strtotime( $comment->comment_date_gmt ), current_time( 'timestamp' ) ) ); ?>
+				&middot; <?php echo esc_attr( human_time_diff( strtotime( $comment->comment_date ), current_time( 'timestamp' ) ) ); ?>
 			</time>
 
 		</div>
