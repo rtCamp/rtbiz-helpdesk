b0447580c5612f72d0fdce35a54c84f6df6a213c
Udit Desai <desaiuditd@gmail.com>
2014-11-12 20:20:10 +0530

    Comment Content Fix

--- app/helper/class-rt-hd-utils.php
+++ app/helper/class-rt-hd-utils.php
@@ -33,15 +33,15 @@ if ( ! class_exists( 'Rt_HD_Utils' ) ) {
 		 * @since rt-Helpdesk 0.1
 		 */
 		static public function force_utf_8( $string ) {
-			return preg_replace('/[^(\x20-\x7F)]*/','', $string);
-			$string = preg_replace( '/[\x00-\x08\x10\x0B\x0C\x0E-\x19\x7F]' .
-									'|(?<=^|[\x00-\x7F])[\x80-\xBF]+' .
-									'|([\xC0\xC1]|[\xF0-\xFF])[\x80-\xBF]*' .
-									'|[\xC2-\xDF]((?![\x80-\xBF])|[\x80-\xBF]{2,})' .
-									'|[\xE0-\xEF](([\x80-\xBF](?![\x80-\xBF]))|(?![\x80-\xBF]{2})|[\x80-\xBF]{3,})/' ,
-			                      '?', $string );
+//			return preg_replace('/[^(\x20-\x7F)]*/','', $string);
+//			$string = preg_replace( '/[\x00-\x08\x10\x0B\x0C\x0E-\x19\x7F]' .
+//									'|(?<=^|[\x00-\x7F])[\x80-\xBF]+' .
+//									'|([\xC0\xC1]|[\xF0-\xFF])[\x80-\xBF]*' .
+//									'|[\xC2-\xDF]((?![\x80-\xBF])|[\x80-\xBF]{2,})' .
+//									'|[\xE0-\xEF](([\x80-\xBF](?![\x80-\xBF]))|(?![\x80-\xBF]{2})|[\x80-\xBF]{3,})/' ,
+//			                      '?', $string );
 
-			$string = preg_replace( '/\xE0[\x80-\x9F][\x80-\xBF]' . '|\xED[\xA0-\xBF][\x80-\xBF]/S','?', $string );
+//			$string = preg_replace( '/\xE0[\x80-\x9F][\x80-\xBF]' . '|\xED[\xA0-\xBF][\x80-\xBF]/S','?', $string );
 
 			return $string;
 		}
