a2fc8a04af4ccdb539d8de9a3eada3bcfae053c5
mehulkaklotar <mehul.kaklotar@gmail.com>
2014-11-27 18:21:33 +0530

    Squashed 'app/vendor/EmailReplyParser/' content from commit 8f13824
    git-subtree-dir: app/vendor/EmailReplyParser
    git-subtree-split: 8f138249f36ee398c7849f97b5374bed94c880aa

---
+++ tests/EmailReplyParser/Tests/TestCase.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace EmailReplyParser\Tests;
+
+class TestCase extends \PHPUnit_Framework_TestCase
+{
+    protected function getFixtures($file)
+    {
+        return file_get_contents(__DIR__ . '/../../Fixtures/' . $file);
+    }
+}
