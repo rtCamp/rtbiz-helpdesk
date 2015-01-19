b2a4deb97f2992594dffb14bdfbee0beaac6500c
Utkarsh Patel <utkarsh.patel@rtcamp.com>
2015-01-19 14:03:52 +0530

    fix dashboard error

--- app/class-rt-wp-helpdesk.php
+++ app/class-rt-wp-helpdesk.php
@@ -83,7 +83,7 @@ if ( ! class_exists( 'RT_WP_Helpdesk' ) ) {
 
 			//Setting
 
-			global $rt_hd_redux_framework_Helpdesk_Config, $rt_hd_import_operation;
+			global $rt_hd_redux_framework_Helpdesk_Config, $rt_hd_import_operation, $rt_hd_offering_support;
 
 			$rt_hd_redux_framework_Helpdesk_Config = new Redux_Framework_Helpdesk_Config();
 			$rt_hd_import_operation                = new Rt_HD_Import_Operation();
