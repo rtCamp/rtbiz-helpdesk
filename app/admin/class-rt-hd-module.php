02f73f62a57fdbc6cca7f13ac2790e6c4459002e
Utkarsh Patel <utkarsh.patel@rtcamp.com>
2015-01-21 13:34:15 +0530

    change in ticket status CSS

--- app/admin/class-rt-hd-module.php
+++ app/admin/class-rt-hd-module.php
@@ -100,19 +100,19 @@ if ( ! class_exists( 'Rt_HD_Module' ) ) {
 					'slug'        => 'hd-unanswered',
 					'name'        => __( 'Unanswered', RT_HD_TEXT_DOMAIN ),
 					'description' => __( 'Ticket is unanswered. It needs to be replied. The default state.', RT_HD_TEXT_DOMAIN ),
-				    'style'       => 'padding: 5px; background: #FDD7E4; color: red; border: 1px solid red; border-radius: 5px;'
+				    'style'       => 'padding: 5px; background: #ECA1A1; color: #FFF; border: 1px solid #ECA1A1; border-radius: 5px; font-weight: bold;',
 				),
 				array(
 					'slug'        => 'hd-answered',
 					'name'        => __( 'Answered', RT_HD_TEXT_DOMAIN ),
 					'description' => __( 'Ticket is answered. Expecting further communication from client', RT_HD_TEXT_DOMAIN ),
-					'style'       => 'padding: 5px; background: #99FF99; color: #006600; border: 1px solid #006600; border-radius: 5px;'
+					'style'       => 'padding: 5px; background: #82CE87; color: #FFF; border: 1px solid #82CE87; border-radius: 5px; font-weight: bold;',
 				),
 				array(
 					'slug'        => 'hd-archived',
 					'name'        => __( 'Archived', RT_HD_TEXT_DOMAIN ),
 					'description' => __( 'Ticket is archived. Client can re-open if they wish to.', RT_HD_TEXT_DOMAIN ),
-					'style'       => 'padding: 5px; background: #CCCCCC; color: #404040; border: 1px solid #404040; border-radius: 5px;'
+					'style'       => 'padding: 5px; background: #9CB8D4; color: #FFF; border: 1px solid #9CB8D4; border-radius: 5px; font-weight: bold;',
 				),
 			);
 
