78368d5501c3381d8ba63d5a43d8c3b912921166
Dipesh Kakadiya <dipesh.kakadiya@rtcamp.com>
2015-02-04 13:25:45 +0530

    fix email template bug

--- app/admin/class-rt-hd-email-notification.php
+++ app/admin/class-rt-hd-email-notification.php
@@ -467,7 +467,7 @@ if ( ! class_exists( 'RT_HD_Email_Notification' ) ) {
 			$title = $this->get_email_title( $post_id, $post_type );
 			$user = get_post_meta( $post_id, '_rtbiz_hd_updated_by', true );
 			$ticket_update_user = get_user_by( 'id', $user );
-			$body = '<br />' . 'Ticket updated by : <strong>' . $ticket_update_user->display_name . '</strong>'. $body;
+			$body = '<br />' . 'Ticket updated by : <strong>' . $ticket_update_user->display_name . '</strong><br/>'. $body;
 			$this->insert_new_send_email( $subject, $title, rthd_get_general_body_template( $body ) , $to, array(), array_unique( $bccemails ), array(), $post_id, 'post' );
 		}
 
