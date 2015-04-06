b2c92158f936a45424831f955cb7c7a92d0496fd
Dipesh Kakadiya <dipesh.kakadiya@rtcamp.com>
2015-04-06 12:00:57 +0530

    Todo removed

--- app/admin/class-rt-hd-import-operation.php
+++ app/admin/class-rt-hd-import-operation.php
@@ -25,7 +25,6 @@ if ( ! class_exists( 'Rt_HD_Import_Operation' ) ) {
 	 * Class Rt_HD_Tickets
 	 * HD : Help Desk
 	 * This class is for tickets related functions
-	 * todo:what this function does ?
 	 *
 	 * @since rt-Helpdesk 0.1
 	 */
@@ -1084,8 +1083,7 @@ if ( ! class_exists( 'Rt_HD_Import_Operation' ) ) {
 		 */
 		public function check_duplicate_comment( $comment_post_ID, $comment_date, $comment_content, $comment_content_old ) {
 			global $wpdb;
-			//todo : 00:00:00 replace with 00:10:00 before live the chanages
-			$sql    = "select * from $wpdb->comments where  comment_post_id=%d  and comment_date between subtime(%s,'00:00:00') and addtime(%s,'00:00:00')";
+			$sql    = "select * from $wpdb->comments where  comment_post_id=%d  and comment_date between subtime(%s,'00:10:00') and addtime(%s,'00:10:00')";
 			$sql    = $wpdb->prepare( $sql, $comment_post_ID, $comment_date, $comment_date );
 			$result = $wpdb->get_results( $sql );
 			global $signature;
