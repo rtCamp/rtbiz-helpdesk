e262a8eebadaaabd959c259ea22560cdf0e468d9
dipeshkakadiya <dipesh.kakadiya@rtcamp.com>
2015-04-29 19:12:01 +0530

    mailbox css change

--- app/admin/class-rt-hd-setup-wizard.php
+++ app/admin/class-rt-hd-setup-wizard.php
@@ -87,9 +87,11 @@ if ( ! class_exists( 'Rt_HD_setup_wizard' ) ) {
 
 				<h1><?php _e( 'Mailbox Setup' ); ?></h1>
 				<fieldset style="display: none">
-					<h3><?php _e( 'Incoming MailBox Setup', RT_BIZ_TEXT_DOMAIN ); ?></h3>
-					<p class="description">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
-					<?php rthd_mailbox_setup_view(); ?>
+					<div class="rthd-setup-wizard-controls">
+						<h3><?php _e( 'Incoming MailBox Setup', RT_BIZ_TEXT_DOMAIN ); ?></h3>
+						<p class="description">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+						<?php rthd_mailbox_setup_view(); ?>
+					</div>
 				</fieldset>
 
 				<h1><?php _e( 'Finish' ); ?></h1>
