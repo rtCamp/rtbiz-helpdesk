1f685c3a0f5f932448b381361857a52aa694b624
dipeshkakadiya <dipesh.kakadiya@rtcamp.com>
2015-05-01 02:19:13 +0530

    dismiss notice issue fixed

--- app/helper/rthd-functions.php
+++ app/helper/rthd-functions.php
@@ -847,7 +847,7 @@ function rthd_admin_notice_dependency_installed(){
 		<div class="updated">
 			<p>
 				<?php echo $string ;?>
-				<a class="welcome-panel-close" style="float: right" href="<?php echo  admin_url( 'edit.php?post_type=rtbiz_hd_ticket&page=rthd-setup-wizard?close_notice=true' ); ?>">Dismiss</a>
+				<a class="welcome-panel-close" style="float: right" href="<?php echo  admin_url( 'edit.php?post_type=rtbiz_hd_ticket&page=rthd-setup-wizard&close_notice=true' ); ?>">Dismiss</a>
 			</p>
 		</div>
 	<?php
