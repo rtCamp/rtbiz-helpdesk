fa7d6655d0256dc0d0c3462c1c23f5a8df9b758b
Utkarsh Patel <utkarsh.patel@rtcamp.com>
2015-05-05 15:52:36 +0530

    common js code updated

--- app/assets/js/vendors/common.js
+++ app/assets/js/vendors/common.js
@@ -33,8 +33,8 @@ jQuery( document ).ready( function () {
 			rthd_common.initEditFollowUp();
 			rthd_common.initLoadAll();
 			rthd_common.initEditContent();
-			rthd_common.initAutoResponseSettings();
 		},
+
 		initAddNewFollowUp : function(){
 			$ticket_unique_id = jQuery( '#ticket_unique_id' ).val();
 			var uploadedfiles= [];
@@ -214,6 +214,218 @@ jQuery( document ).ready( function () {
 				             } );
 			}
 		},
+
+		initEditFollowUp: function () {
+			var commentid;
+			jQuery("#delfollowup" ).click(function() {
+				var r = confirm( "Are you sure you want to remove this Followup?" );
+				if ( r != true ) {
+					e.preventDefault();
+					return false;
+				}
+				jQuery('#edithdspinner' ).show();
+				jQuery(this).attr('disabled','disabled');
+				postid= jQuery('#post-id' ).val();
+				jQuery.ajax( { url: ajaxurl,
+					             type: 'POST',
+					             dataType: 'json',
+					             data: {
+						             action: 'helpdesk_delete_followup',
+						             comment_id: commentid,
+						             post_id : postid
+					             },
+					             success: function ( data ) {
+						             if ( data.status ) {
+							             jQuery( "#comment-" + commentid ).fadeOut( 500, function () {
+								             jQuery( this ).remove();
+							             } );
+							             jQuery( '.close-edit-followup' ).trigger( 'click' );
+						             } else {
+							             alert( "Error while deleting comment from server" );
+						             }
+						             jQuery('#edithdspinner' ).hide();
+						             jQuery("#delfollowup" ).removeAttr('disabled');
+					             },
+					             error: function ( xhr, textStatus, errorThrown ) {
+						             alert( "error while removing follow up." );
+						             jQuery('#edithdspinner').hide();
+						             jQuery("#delfollowup" ).removeAttr('disabled');
+					             }
+				             } );
+
+			});
+			jQuery(document).on('click', '.close-edit-followup', function (e){
+				e.preventDefault();
+				jQuery('#dialog-form' ).slideToggle('slow');
+				jQuery('#new-followup-form' ).show();
+				jQuery(document).scrollTop( ( jQuery('#comment-'+commentid ).offset().top ) );
+			});
+			jQuery( document ).on('click', '.editfollowuplink',function(e){
+				e.preventDefault();
+				var select =jQuery(this ).parents();
+				rthd_common.rthd_tinymce_set_content( 'editedfollowupcontent', jQuery(this).parents().siblings('.rthd-comment-content').data('content'));
+				commentid=select.siblings('#followup-id' ).val();
+				var that = select.siblings( '#is-private-comment' ).val();
+				jQuery('#edit-private' ).val(that);
+				jQuery('#new-followup-form' ).hide();
+				if ( ! jQuery('#dialog-form').is(":visible")){
+					jQuery('#dialog-form' ).slideToggle('slow');
+				}
+				if ( jQuery('#edit-ticket-data').is(":visible")){
+					jQuery('#edit-ticket-data' ).slideToggle('slow');
+				}
+				jQuery(document).scrollTop( ( jQuery('#dialog-form').offset().top ) - 50 );
+
+			} );
+
+			jQuery("#editfollowup" ).click(function(){
+				var requestArray = new Object();
+				var content =  rthd_common.rthd_tinymce_get_content( 'editedfollowupcontent' );
+				if (! content){
+					alert("Please enter comment");
+					return false;
+				}
+				if (content.replace(/\s+/g, " ") === jQuery('#comment-'+commentid ).find('.rthd-comment-content' ).data('content') ){
+					alert('You have not edited comment!');
+					return false;
+				}
+				jQuery('#edithdspinner' ).show();
+				jQuery(this).attr('disabled','disabled');
+				requestArray['post_type'] = rthd_post_type;
+				requestArray["comment_id"] = commentid ;
+				requestArray["action"] = "rthd_update_followup_ajax";
+				requestArray['followuptype']="comment";
+				requestArray['followup_ticket_unique_id']=jQuery('#ticket_unique_id' ).val();
+				//requestArray['followup_private']='no';
+				requestArray['followup_post_id']=jQuery('#post-id' ).val();
+				requestArray['followup_private']= jQuery('#edit-private').val();
+				requestArray["followuptype"] = 'comment';
+				//requestArray["followup_post_id"] = jQuery( "#ticket_id" ).val();
+				//requestArray["follwoup-time"] = jQuery( "#follwoup-time" ).val();
+				requestArray["followup_content"] = content;
+				jQuery.ajax(
+					{
+						url: ajaxurl,
+						dataType: "json",
+						type: 'post',
+						data: requestArray,
+						success: function ( data ) {
+							if ( data.status ) {
+								jQuery('#comment-'+commentid ).replaceWith(data.comment_content);
+								jQuery( '.close-edit-followup' ).trigger( 'click' );
+								jQuery(document).scrollTop( ( jQuery('#comment-'+commentid ).offset().top ) - 50 );
+							} else {
+								alert( data.message );
+							}
+							jQuery('#edithdspinner' ).hide();
+							jQuery("#editfollowup" ).removeAttr('disabled');
+
+						},
+						error: function (data){
+							alert("Sorry :( something went wrong!");
+							jQuery('#edithdspinner' ).hide();
+							jQuery("#editfollowup" ).removeAttr('disabled');
+						}
+					} );
+			});
+		},
+
+		initLoadAll: function(){
+			jQuery('#followup-load-more, .load-more-block' ).click(function (e){
+				e.preventDefault();
+				var requestArray = new Object();
+				var totalcomment=parseInt( jQuery('#followup-totalcomment' ).val(),10);
+				var limit = parseInt(jQuery('#followup-limit').val(),10);
+				if( limit != 3 ){
+					return;
+				}
+				jQuery(this ).parent().hide();
+				jQuery('#load-more-hdspinner' ).show();
+				requestArray['limit']=totalcomment-3;
+				requestArray['offset']=0;
+				requestArray["action"] = "load_more_followup";
+				requestArray['post_id'] =  jQuery('#post-id' ).val();
+				//requestArray['all'] =  'true';
+				jQuery.ajax( {
+					             url: ajaxurl,
+					             dataType: "json",
+					             type: 'post',
+					             data: requestArray,
+					             success: function ( data ) {
+						             if (data.status) {
+							             jQuery( '#followup-offset' ).val( data.offset );
+							             jQuery( '#chat-UI' ).prepend( data.comments );
+						             }
+						             jQuery('#load-more-hdspinner' ).hide();
+					             },
+					             error: function(){
+						             jQuery('#load-more-hdspinner' ).hide();
+						             return false;
+					             }
+				             });
+
+			});
+		},
+
+		initEditContent: function(){
+			jQuery('.edit-ticket-link' ).click(function(e){
+				e.preventDefault();
+				jQuery('#edit-ticket-data' ).slideToggle('slow');
+				if ( jQuery('#dialog-form').is(":visible")){
+					jQuery('#dialog-form' ).slideToggle('slow');
+				}
+				if ( ! jQuery('#edit-ticket-data').is(":visible")){
+					jQuery('#edit-ticket-data' ).slideToggle('slow');
+				}
+				jQuery('#new-followup-form' ).hide();
+				jQuery(document).scrollTop( ( jQuery('#edit-ticket-data').offset().top ) - 50 );
+				rthd_common.rthd_tinymce_set_content( 'editedticketcontent', jQuery(this).closest('.ticketcontent').find('.rthd-comment-content' ).data('content') );
+			});
+			jQuery('.close-edit-content' ).click(function(e){
+				e.preventDefault();
+				jQuery('#edit-ticket-data' ).slideToggle('slow');
+				jQuery('#new-followup-form' ).show();
+				jQuery(document).scrollTop( ( jQuery('.ticketcontent').offset().top ) - 50 );
+			});
+
+			jQuery('#edit-ticket-content-click' ).click(function(){
+				jQuery('#edit-ticket-data' ).slideToggle('slow');
+				jQuery('#new-followup-form' ).hide();
+				var requestArray = new Object();
+				requestArray['action']= 'rthd_add_new_ticket_ajax';
+				requestArray['post_id']= jQuery('#post-id' ).val();
+				var content = rthd_common.rthd_tinymce_get_content( 'editedticketcontent' );
+				requestArray['body']= content;
+				requestArray['nonce']= jQuery('#edit_ticket_nonce' ).val();
+				jQuery('#ticket-edithdspinner' ).show();
+				jQuery(this).attr('disabled','disabled');
+				jQuery.ajax( {
+					             url: ajaxurl,
+					             type: 'POST',
+					             dataType: 'json',
+					             data: requestArray,
+					             success: function ( data ) {
+						             if(data.status){
+							             jQuery('#ticket-edithdspinner' ).hide();
+							             jQuery("#edit-ticket-content-click" ).removeAttr('disabled');
+							             jQuery('.edit-ticket-link' ).closest('.ticketcontent' ).find('.rthd-comment-content' ).html( rthd_common.rthd_tinymce_get_content( 'editedticketcontent' ) );
+							             jQuery('#edit-ticket-data' ).hide();
+							             jQuery('#new-followup-form' ).slideToggle('slow');
+							             jQuery(document).scrollTop( ( jQuery('.ticketcontent').offset().top ) - 50 );
+						             }
+						             else{
+							             console.log(data.msg);
+						             }
+					             },
+					             error: function ( xhr, textStatus, errorThrown ) {
+						             alert( "Error" );
+						             jQuery('#ticket-edithdspinner' ).hide();
+						             jQuery("#edit-ticket-content-click" ).removeAttr('disabled');
+					             }
+				             });
+			});
+		}
+
 	}
 
 });
