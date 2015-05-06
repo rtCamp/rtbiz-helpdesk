3b1aeebea9b4da974b6dc71aecfee64d7a27b9b9
dipeshkakadiya <dipesh.kakadiya@rtcamp.com>
2015-05-06 15:18:39 +0530

    team menu updated

--- app/admin/class-rt-hd-admin.php
+++ app/admin/class-rt-hd-admin.php
@@ -53,6 +53,7 @@ if ( ! class_exists( 'Rt_HD_Admin' ) ) {
 			if ( rthd_check_wizard_completed() ) {
 				add_submenu_page( 'edit.php?post_type=' . Rt_HD_Module::$post_type, __( 'Customer ' ), __( 'Customer' ), rt_biz_get_access_role_cap( RT_HD_TEXT_DOMAIN, 'admin' ), 'edit.php?post_type=' . rt_biz_get_contact_post_type() . '&rt_contact_group=customer' );
 				add_submenu_page( 'edit.php?post_type=' . Rt_HD_Module::$post_type, __( 'Staff' ), __( 'Staff' ), rt_biz_get_access_role_cap( RT_HD_TEXT_DOMAIN, 'admin' ), 'edit.php?post_type=' . rt_biz_get_contact_post_type() . '&rt_contact_group=staff' );
+				add_submenu_page( 'edit.php?post_type=' . esc_html( Rt_HD_Module::$post_type ), __( '---Teams' ), __( '---Teams' ), rt_biz_get_access_role_cap( RT_BIZ_TEXT_DOMAIN, 'editor' ), 'edit-tags.php?taxonomy=' . RT_Departments::$slug . '&post_type=' . Rt_HD_Module::$post_type );
 				/* add_submenu_page( 'edit.php?post_type=' . esc_html( Rt_HD_Module::$post_type ), __( 'Companies' ), __( '--- Companies' ), rt_biz_get_access_role_cap( RT_HD_TEXT_DOMAIN, 'admin' ), 'edit.php?post_type=' . rt_biz_get_company_post_type() );
 				  add_submenu_page( 'edit.php?post_type=' . esc_html( Rt_HD_Module::$post_type ), __( 'Access Control' ), __( '--- Access Control' ), rt_biz_get_access_role_cap( RT_HD_TEXT_DOMAIN, 'admin' ), Rt_Biz::$access_control_slug, array(
 				  $rt_access_control,
@@ -61,7 +62,7 @@ if ( ! class_exists( 'Rt_HD_Admin' ) ) {
 
 				/* $contact_groups_label = apply_filters( 'rtbiz_contact_groups_menu_item_label', __( 'Contact Groups' ) );
 				  add_submenu_page( 'edit.php?post_type=' . esc_html( Rt_HD_Module::$post_type ), $contact_groups_label, '--- ' . $contact_groups_label, rt_biz_get_access_role_cap( RT_BIZ_TEXT_DOMAIN, 'editor' ), 'edit-tags.php?taxonomy=' . Rt_Contact::$user_category_taxonomy . '&post_type=' . Rt_HD_Module::$post_type ); */
-				/* add_submenu_page( 'edit.php?post_type=' . esc_html( Rt_HD_Module::$post_type ), __( 'Teams' ), __( 'Teams' ), rt_biz_get_access_role_cap( RT_BIZ_TEXT_DOMAIN, 'editor' ), 'edit-tags.php?taxonomy=' . RT_Departments::$slug . '&post_type=' . Rt_HD_Module::$post_type ); */
+				/*  */
 			}
 		}
 
