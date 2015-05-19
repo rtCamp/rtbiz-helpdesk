8e46e912e39076d03fbb1fd7780e8de07294e289
dipeshkakadiya <dipesh.kakadiya@rtcamp.com>
2015-05-19 13:54:57 +0530

    Change contact update limit

--- app/admin/class-rt-hd-setup-wizard.php
+++ app/admin/class-rt-hd-setup-wizard.php
@@ -684,7 +684,7 @@ if ( ! class_exists( 'Rt_HD_setup_wizard' ) ) {
 		 */
 		function import_all_users() {
 			$arrReturn = array( 'status' => false );
-			$LIMIT = 5; //todo change this limit to 50
+			$LIMIT = 50;
 			if ( ! empty( $_POST['import'] ) && ! empty( $_POST['nonce'] ) && wp_verify_nonce( $_POST['nonce'], get_current_user_id() . 'import-all-users' ) ) {
 				global $wpdb;
 				$helpdesk_users = rthd_get_helpdesk_user_ids();
