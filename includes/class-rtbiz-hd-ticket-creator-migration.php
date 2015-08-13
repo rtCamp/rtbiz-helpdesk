0a1d00ff54b64c74685adac4727d9e7114c39dd5
utkarsh <utkarsh.patel@rtcamp.com>
2015-08-13 12:10:20 +0530

    migration change to single SQLs

--- includes/class-rtbiz-hd-ticket-creator-migration.php
+++ includes/class-rtbiz-hd-ticket-creator-migration.php
@@ -39,23 +39,39 @@ if ( ! class_exists( 'Rtbiz_HD_Ticket_Creator_Migration' ) ) {
 			//*****************Start Ticket post meta migration********************
 
 			// get unique created by users
-			$ticket_creators = $wpdb->get_col('SELECT DISTINCT meta_value
-					FROM '.$wpdb->postmeta.'
-					WHERE meta_key = "_rtbiz_hd_created_by"');
+//			$ticket_creators = $wpdb->get_col('SELECT DISTINCT meta_value
+//					FROM '.$wpdb->postmeta.'
+//					WHERE meta_key = "_rtbiz_hd_created_by"');
 
 			// update each user using SQL update
-			foreach ( $ticket_creators as $creator ) {
-				if ( ( (string)(int)$creator === $creator ) && (int)$creator > 0  ) {
-					$creator_contact_id = rtbiz_hd_get_contact_id_by_user_id( $creator, true );
-					if ( ! empty( $creator_contact_id ) ) {
-						$wpdb->update( $wpdb->postmeta, array( 'meta_value' => $creator_contact_id ), array( 'meta_key'   => '_rtbiz_hd_created_by',
-						                                                                                     'meta_value' => $creator,
-						), array( '%d' ), array( '%s', '%d' ) );
-					} else {
-						error_log('[Meta migration] Ticket creator user id :'.$creator. " Not found");
-					}
-				}
-			}
+//			foreach ( $ticket_creators as $creator ) {
+//				if ( ( (string)(int)$creator === $creator ) && (int)$creator > 0  ) {
+//					$creator_contact_id = rtbiz_hd_get_contact_id_by_user_id( $creator, true );
+//					if ( ! empty( $creator_contact_id ) ) {
+//						$wpdb->update( $wpdb->postmeta, array( 'meta_value' => $creator_contact_id ), array( 'meta_key'   => '_rtbiz_hd_created_by',
+//						                                                                                     'meta_value' => $creator,
+//						), array( '%d' ), array( '%s', '%d' ) );
+//					} else {
+//						error_log('[Meta migration] Ticket creator user id :'.$creator. " Not found");
+//					}
+//				}
+//			}
+			$q1 = $wpdb->prepare( "UPDATE
+					{$wpdb->postmeta} pm
+						JOIN
+					{$wpdb->prefix}p2p p2p
+						ON (
+							p2p.p2p_to = pm.meta_value
+								AND
+							pm.meta_key = %s
+								AND
+							p2p.p2p_type = %s
+							)
+					SET pm.meta_value =  p2p.p2p_from",
+				'_rtbiz_hd_created_by',
+				'contact_to_user'
+			);
+			$wpdb->query( $q1 );
 
 			//*****************End Ticket post meta migration********************
 
@@ -63,7 +79,7 @@ if ( ! class_exists( 'Rtbiz_HD_Ticket_Creator_Migration' ) ) {
 			//*****************Start Ticket Index table migration*****************
 			$table_name = rtbiz_hd_get_ticket_table_name();
 
-			$ticket_creators_index = $wpdb->get_col( 'SELECT DISTINCT user_created_by FROM '.$table_name ); // get unique user_creator id so we save update queries
+			/*$ticket_creators_index = $wpdb->get_col( 'SELECT DISTINCT user_created_by FROM '.$table_name ); // get unique user_creator id so we save update queries
 			foreach ( $ticket_creators_index as $creator ) {
 				if ( ( (string)(int) $creator === $creator ) && (int)$creator > 0 ) {
 					$creator_contact_id = rtbiz_hd_get_contact_id_by_user_id( $creator, true );
@@ -74,13 +90,27 @@ if ( ! class_exists( 'Rtbiz_HD_Ticket_Creator_Migration' ) ) {
 						error_log("[$table_name migration] Ticket creator user id :".$creator. " Not found");
 					}
 				}
-			}
+			}*/
+
+			$q2 = $wpdb->prepare( "UPDATE
+				{$table_name} ti
+					JOIN
+				{$wpdb->prefix}p2p p2p
+					ON (
+						p2p.p2p_to = ti.user_created_by
+							AND
+						p2p.p2p_type = %s
+						)
+				SET ti.user_created_by =  p2p.p2p_from",
+				'contact_to_user'
+			);
+			$wpdb->query( $q2 );
 
 			//*****************End Ticket Index table migration*****************
 
 
 			//*****************Start Follow up user migration*****************
-			$followup_authors = $wpdb->get_results( 'SELECT comment.user_id as user_id, comment.comment_ID as ID FROM '.$wpdb->comments.' as comment JOIN '.$wpdb->posts.' as posts ON (comment.comment_post_ID = posts.ID) WHERE posts.post_type="'.Rtbiz_HD_Module::$post_type.'"' ); // add existing followup creator to comment meta
+/*			$followup_authors = $wpdb->get_results( 'SELECT comment.user_id as user_id, comment.comment_ID as ID FROM '.$wpdb->comments.' as comment JOIN '.$wpdb->posts.' as posts ON (comment.comment_post_ID = posts.ID) WHERE posts.post_type="'.Rtbiz_HD_Module::$post_type.'"' ); // add existing followup creator to comment meta
 			$followup_authors = array_filter( $followup_authors );
 			foreach ( $followup_authors as $followup ) {
 				if ( ! empty($followup->ID ) && ! empty($followup->user_id ) ) {
@@ -91,7 +121,32 @@ if ( ! class_exists( 'Rtbiz_HD_Ticket_Creator_Migration' ) ) {
 						error_log("[comment meta migration] Ticket creator user id :".$followup->user_id. " Not found");
 					}
 				}
-			}
+			}*/
+			$q3 = $wpdb->prepare( "
+					INSERT INTO
+						{$wpdb->commentmeta}
+							(comment_id,meta_key,meta_value)
+						values (
+									SELECT
+										cmt.comment_post_ID, %s, p2p.p2p_from
+											FROM
+											{$wpdb->comments} cmt
+											JOIN
+											{$wpdb->prefix}p2p p2p
+												ON ( p2p.p2p_to = cmt.user_id AND p2p.p2p_type = %s  )
+											JOIN
+											{$wpdb->posts} posts
+												ON (
+														cmt.comment_post_ID = posts.ID
+													AND
+														posts.post_type = %s
+													)
+								)",
+				'_rtbiz_hd_followup_author',
+				'contact_to_user',
+				'ticket'
+			);
+			$wpdb->query( $q3 );
 			//*****************End Follow up user migration*****************
 
 			// script is been performed telling that to db so next time this will not run.
