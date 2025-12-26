309a2f2a1c8f953967ee85752f9d99dd060e4652
Mohammed Noumaan Ahamed <mohamed.ahamed@rtcamp.com>
2025-12-26 18:47:40 +0530

    fix: correct regex replacement in getParameterByName function

--- admin/js/rt-custom-status.js
+++ admin/js/rt-custom-status.js
@@ -25,7 +25,7 @@ jQuery( document ).ready(function () {
 	}
 
 	function getParameterByName(name) {
-		name = name.replace( /[\[]/, "\\[" ).replace( /[\]]/, "\\]" );
+		name = name.replace( /[\[]/g, "\\[" ).replace( /[\]]/g, "\\]" );
 		var regex = new RegExp( "[\\?&]" + name + "=([^&#]*)" ),
 			results = regex.exec( location.search );
 		return results === null ? "" : decodeURIComponent( results[1].replace( /\+/g, " " ) );
